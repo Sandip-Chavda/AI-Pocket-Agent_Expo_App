109b40bc2654e519b31c0e58a4ba673c9e582ce6
Sandip-Chavda <chavdas766@gmail.com>
2025-09-19 11:02:00 +0530

    Clerk OAuth authentication integrated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </ClerkProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,14 @@
 import { COLORS } from "@/constants/colors";
+import { useAuth, useSSO, useUser } from "@clerk/clerk-expo";
 import { Fontisto } from "@expo/vector-icons";
+import * as AuthSession from "expo-auth-session";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import { useCallback, useEffect, useState } from "react";
 import {
+  ActivityIndicator,
   Dimensions,
   StyleSheet,
   Text,
@@ -11,7 +17,83 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Preloads the browser for Android devices to reduce authentication load time
+    // See: https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      // Cleanup: closes browser when component unmounts
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function Index() {
+  const { isSignedIn } = useAuth();
+  const { user, isLoaded } = useUser();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
+
+  // console.log(user?.primaryEmailAddress?.emailAddress);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      //TODO: Redirect to Home screen.
+    }
+    if (isSignedIn != undefined) {
+      setLoading(false);
+    }
+  }, [isSignedIn]);
+
+  useWarmUpBrowser();
+
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO();
+
+  const onLoginPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } =
+        await startSSOFlow({
+          strategy: "oauth_google",
+          // For web, defaults to current path
+          // For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+          // For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+          redirectUrl: AuthSession.makeRedirectUri(),
+        });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              // Check for tasks and navigate to custom UI to help users resolve them
+              // See https://clerk.com/docs/custom-flows/overview#session-tasks
+              console.log(session?.currentTask);
+              return;
+            }
+
+            router.push("/");
+          },
+        });
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <LinearGradient
@@ -35,10 +117,14 @@ export default function Index() {
         </Text>
       </View>
 
-      <TouchableOpacity style={styles.button}>
-        <Fontisto name="google" size={22} color="white" />
-        <Text style={styles.buttonText}>Google Login</Text>
-      </TouchableOpacity>
+      {!loading && (
+        <TouchableOpacity style={styles.button} onPress={onLoginPress}>
+          <Fontisto name="google" size={22} color="white" />
+          <Text style={styles.buttonText}>Login with Google</Text>
+        </TouchableOpacity>
+      )}
+
+      {loading && <ActivityIndicator size={"large"} color={COLORS.primary} />}
     </SafeAreaView>
   );
 }
